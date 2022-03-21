0d37b1ce1fd185ea5596ad1209f9628abda08b1e
parkkhee <76894305+parkkhee@users.noreply.github.com>
2022-03-21 11:41:49 +0900

    Create 1000.java

---
+++ 1000.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] arg){
+        int A,B;
+        
+        Scanner scan = new Scanner(System.in);
+        A= scan.nextInt();
+        B= scan.nextInt();
+        
+        System.out.print(A+B);
+    }
+}
