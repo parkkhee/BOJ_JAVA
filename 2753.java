1aa3077ea0860fbc3a00356023c49d7a8769bc2e
parkkhee <76894305+parkkhee@users.noreply.github.com>
2022-07-11 13:17:02 +0900

    Create 2753.java

---
+++ 2753.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] arg){
+        int year;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        year = scan.nextInt();
+        
+        if(year%4==0 && (year%100 !=0 || year%400 ==0)){
+            System.out.print(1);
+        }
+        else{
+            System.out.print(0);
+        }
+        
+    }
+}
