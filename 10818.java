459118020ff72094ab7918d3d617a2e6a202ce7d
parkkhee <76894305+parkkhee@users.noreply.github.com>
2022-07-11 13:10:44 +0900

    Create 10818.java

---
+++ 10818.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FastReader fr = new FastReader(); // 문제 해결 소스 코드
+        int a = fr.nextInt();
+        
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        
+        for(int i=0; i<a; i++){
+            int x = fr.nextInt();
+            arrayList.add(x);
+        }
+        int nummax, nummin;
+        nummax=Collections.min(arrayList);
+        nummin=Collections.max(arrayList);
+        
+        System.out.print(nummax+" "+nummin);
+                
+    }
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException { br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}
