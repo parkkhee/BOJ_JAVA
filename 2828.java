376954a9ebdfadd19041061c9f484e64308e8345
June <76894305+parkkhee@users.noreply.github.com>
2023-03-08 00:19:01 +0900

    S5 / 00:20:20 / X

---
+++ 2828.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws IOException {
+	
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		
+		int N = Integer.parseInt(st.nextToken()); 
+		int M = Integer.parseInt(st.nextToken()); 
+		int J = Integer.parseInt(br.readLine()); 
+		
+		int cur = 1; 
+
+		for(int i=0; i<J; i++) {
+			r = cur + M - 1; 
+			tmp = Integer.parseInt(br.readLine()); 
+			
+      
+      
+		}
+		System.out.println();
+		
+		
+	}
+	
+	
+	
+	
+	
+}
